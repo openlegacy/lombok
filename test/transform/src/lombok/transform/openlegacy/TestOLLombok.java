859cde0777d2433e69fe022dc64a3a494560e3d8
Matvey Mitnitsky <matvey.m@openlegacy.com>
2017-08-02 18:34:17 +0300

    OL-2710 #comment fixed supportTerminalData issue

--- test/transform/src/lombok/transform/openlegacy/TestOLLombok.java
+++ test/transform/src/lombok/transform/openlegacy/TestOLLombok.java
@@ -6,8 +6,8 @@ import org.junit.runners.Suite;
 // You need to add the following vm-parameter to run the test:
 // -javaagent:dist/openlegacy-lombok.jar -Dshadow.override.lombok=${project_loc:openlegacy-lombok}/bin;${project_loc:openlegacy-lombok}/lib/runtime/*
 // -Ddelombok.bootclasspath=lib/oracleJDK8Environment/rt.jar
-//  also you need to attach openlegacy-openlegacy-api.jar to the test launch to get use of openlegacy annotaions
-// and javax.persistence.jar to test persistence annotations
+//  also you need to attach openlegacy-core.jar to the test launch to get use of openlegacy annotaions
+// add javax.persistence.jar, jackson-annotations.jar and persistance-api.jar to compile appropriate annotations; 
 // When running inside eclipse's junit tester, you don't actually need to run 'ant dist' after updating code, though.
 
 /**
